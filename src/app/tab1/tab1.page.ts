189c8a3b0b2792f383858659c0ec95bdd8fbc806
Fernando Leme Silva <fernandoleme.2004@gmail.com>
2021-12-12 17:16:16 -0300

    Mais componentes, mas programáveis (.ts)

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,38 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
+
+  async apresentarAlertaFavorito() {
+    const alert = await this.alertController.create({
+      header: 'Alerta!',
+      message: 'Esse seria o seu favorito!?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        },
+        {
+          text: 'Confere',
+          handler: () => {
+            this.apresentarToast();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async apresentarToast() {
+    const toast = await this.toastController.create({
+      message: 'Filme preferido favoritado!',
+      duration: 2000,
+      color: 'secondary'
+    });
+    toast.present();
+  }
 
 }
